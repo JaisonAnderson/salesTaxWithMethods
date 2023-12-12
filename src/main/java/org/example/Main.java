d9a275e9692994566c969ee2c066e2641d91897a
Jaiso <Jaisonanderson04@gmail.com>
2023-12-12 13:18:28 -0600

    Creating method names, and declaring globals

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
        /*Setup any variables you need and call your methods
        * here in the main method */
+        final double COUNTY_TAX_RATE = 0.02;
+        final double STATE_TAX_RATE = 0.04;
     }
 
     /* Write a method called inputPurchasePrice that
@@ -11,17 +15,32 @@ public class Main {
     *  a double, and returns the price as a double
     * */
 
+    public static void inputPurchasePrice(){
+        Scanner userInput = new Scanner(System.in);
+
+    }
+
     /* Write a method called calculateTax that
      * accepts a double called price and a double called
      * tax rate as parameters and calculates and returns the tax
      * */
+    public static void calculateTax(){
+
+    }
 
     /* Write a method called calculateTotal that
      * accepts two double values and returns the sum
      * */
 
+    public static void calculateTotal(){
+
+    }
+
     /* Write a method called displayTotals that takes
     *  purchasePrice, stateTax, countyTax, totalTax and totalPrice
     *  as double parameters. Output using decimal formatter amd printf */
 
+    public static void displayTotals(){
+
+    }
 }
